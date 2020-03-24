c5ee1e72c99f19983a86bc2f32e2132a1bd75f85
gaucho <giditre@gmail.com>
2020-03-24 16:20:23 +0100

    commit from gauchonuc1 on 24/03/2020 at 16:20

--- fnode_iaas.py
+++ fnode_iaas.py
@@ -43,10 +43,6 @@ class FogApplicationList(Resource):
 
 class FogApplication(Resource):
 
-  def __init__(self, *args, **kwargs):
-    super(FogApplication, self).__init__()
-    self.fna = kwargs["fna"]
-
   def post(self, app_id):
     # retrieve information from POST body
     req_json = request.get_json(force=True)
@@ -74,7 +70,8 @@ class FogApplication(Resource):
 
     # TODO IMPORTANT find a smarter way to implement command to stress
     if image_uri == "progrium/stress":
-      command = "stress --cpu 1 --io 1 --vm 1 --vm-bytes 1G --timeout 36000s"
+      #command = "stress --cpu 1 --io 1 --vm 1 --vm-bytes 1G --timeout 36000s"
+      command = "stress --cpu 1 --timeout 36000s"
 
     logger.debug("Deploying app {} in container {} with image {}{}".format(app_id,
       image_uri,
@@ -125,25 +122,6 @@ def wait_for_remote_endpoint(ep_address, ep_port):
     logger.warning("Remote endpoint not ready (reponse code {}), retrying soon...".format(resp_code))
     sleep(random.randint(5,15))
 
-#class FogNodeIaaS(threading.Thread):
-#  def __init__(self, ep_address, ep_port, repo_address, repo_port, logger=None):
-#    super().__init__()
-#    self.ep_address = ep_address
-#    self.ep_port = ep_port
-#    self.repo_address = repo_address
-#    self.repo_port = repo_port
-#    self.logger = logger
-#
-#    ### API definition
-#    self.app = Flask(__name__)
-#    self.api = Api(self.app)
-#    self.api.add_resource(Test, '/test')
-#    self.api.add_resource(FogApplication, '/app')
-#
-#  def run(self):
-#    self.app.run(host=ep_address, port=ep_port, debug=True)
-    
-
 ### MAIN
 
 if __name__ == '__main__':
@@ -168,9 +146,6 @@ if __name__ == '__main__':
   parser.add_argument("--collector-port", help="Collector endpoint TCP port, dafault: 5003", nargs="?", default=5003)
   parser.add_argument("--repo-address", help="Image repo endpoint IP address, default: 127.0.0.1", nargs="?", default="127.0.0.1")
   parser.add_argument("--repo-port", help="Image repo endpoint TCP port, default: 5006", nargs="?", default=5006)
-  parser.add_argument("-n", "--num-agents", help="Number of FogNodeAgents to spawn, default: 1", type=int, nargs="?", default=1)
-  parser.add_argument("-l", "--limit-updates", help="Number of updates to send before quitting, default: 0 (infinite)", type=int, nargs="?", default=0)
-  parser.add_argument("-i", "--update-interval", help="Update interval in seconds, default: 10", type=int, nargs="?", default=10)
   parser.add_argument("-w", "--wait-remote", help="Wait for remote endpoint(s), default: false", action="store_true", default=False)
   
   args = parser.parse_args()
@@ -181,37 +156,20 @@ if __name__ == '__main__':
   collector_port = args.collector_port
   repo_address = args.repo_address
   repo_port = args.repo_port
-  n_agents = args.num_agents
-  lim_updates = args.limit_updates
-  interval = args.update_interval
   wait_remote = args.wait_remote
 
   if wait_remote:
     wait_for_remote_endpoint(collector_address, collector_port)
     wait_for_remote_endpoint(repo_address, repo_port)
 
-  fna = fnagent.FogNodeAgent(collector_address, collector_port, node_ipv4=ep_address, name="_FNA{:03d}_".format(0), node_class="I", logger=logger, lim=lim_updates, period=interval)
-
   ### API definition
+
   app = Flask(__name__)
   api = Api(app)
   api.add_resource(Test, '/test')
   api.add_resource(FogNodeInfo, "/info")
   api.add_resource(FogApplicationList, '/apps')
-  api.add_resource(FogApplication, '/app/<app_id>', resource_class_kwargs={"fna":fna})
-
-  #fnode_iaas = FogNodeIaaS(ep_address, ep_port, repo_address, repo_port, logger=logger)
-  #fnode_iaas.start()
-
-  #fna.start()
+  api.add_resource(FogApplication, '/app/<app_id>')
 
   app.run(host=ep_address, port=ep_port, debug=True)
   
-  #try:
-  #  while True:
-  #    sleep(1)
-  #except KeyboardInterrupt:
-  #  docker_client.containers.get("apache").stop()
-  #  docker_client.containers.prune()
-    
-
