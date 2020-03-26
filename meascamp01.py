65d364f9dcd6f83fe618bd75e7eab3990b2e6cea
giditre <giditre@gmail.com>
2020-03-26 15:01:34 +0000

    commit from gauchoforch on 26/03/2020 at 15:01

--- meascamp01.py
+++ meascamp01.py
@@ -3,22 +3,26 @@ import requests
 import json
 import datetime
 
+def print_flush(*args, **kwargs):
+  kwargs["flush"] = True
+  print(*args, **kwargs)
+
 #curl -X DELETE http://127.0.0.1:5003/nodes
-print("DELETE", "http://127.0.0.1:5003/nodes")
+print_flush("DELETE", "http://127.0.0.1:5003/nodes")
 r = requests.delete("http://127.0.0.1:5003/nodes")
-print("Sleep for 240 seconds")
+print_flush("Sleep for 240 seconds")
 time.sleep(240)
 
 #curl http://192.168.10.117:5001/apps
-print("GET", "http://192.168.10.117:5001/nodes")
+print_flush("GET", "http://192.168.10.117:5001/nodes")
 r = requests.get("http://192.168.10.117:5001/apps")
 time.sleep(1)
 
 # request the allocation of new service
-print("GET", "http://192.168.10.117:5001/app/FA002")
+print_flush("GET", "http://192.168.10.117:5001/app/FA002")
 r = requests.get("http://192.168.10.117:5001/app/FA002")
 resp_json = r.json()
-print(json.dumps(resp_json, indent=2))
+print_flush(json.dumps(resp_json, indent=2))
 time.sleep(1)
 
 while r.status_code in [200, 201]:
@@ -29,34 +33,34 @@ while r.status_code in [200, 201]:
   # start new service
   url = "http://{}:{}/app/FA002".format(node_ip, serv_port)
   data_json = {"timeout":10000, "cpu":1}
-  print("POST", data_json, url)
+  print_flush("POST", data_json, url)
   r = requests.post(url, json=data_json)
   resp_json = r.json()
-  print(json.dumps(resp_json, indent=2))
+  print_flush(json.dumps(resp_json, indent=2))
   time.sleep(1)
 
-  print("Sleep for 120 seconds")
+  print_flush("Sleep for 120 seconds")
   time.sleep(120)
 
   # request the allocation of new service
-  print("GET", "http://192.168.10.117:5001/app/FA002")
+  print_flush("GET", "http://192.168.10.117:5001/app/FA002")
   r = requests.get("http://192.168.10.117:5001/app/FA002")
   resp_json = r.json()
-  print(json.dumps(resp_json, indent=2))
+  print_flush(json.dumps(resp_json, indent=2))
   time.sleep(1)
 
-print("Sleep for 240 seconds")
+print_flush("Sleep for 240 seconds")
 time.sleep(240)
 
 # get measurements
-print("GET", "http://127.0.0.1:5003/meas")
+print_flush("GET", "http://127.0.0.1:5003/meas")
 r = requests.get("http://127.0.0.1:5003/meas")
 time.sleep(1)
 with open("../gauchotest/get_meas_test_{0:%Y%m%d_%H%M%S}.json".format(datetime.datetime.now()), "w") as f:
   json.dump(r.json(), f)
 
 #curl -X DELETE http://127.0.0.1:5003/nodes
-print("DELETE", "http://127.0.0.1:5003/nodes")
+print_flush("DELETE", "http://127.0.0.1:5003/nodes")
 r = requests.delete("http://127.0.0.1:5003/nodes")
 time.sleep(1)
 
