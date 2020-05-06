68c5dc4233ba5a574e53ea428889cc18b33dc16e
giditre <giditre@gmail.com>
2020-05-06 13:20:21 +0200

    commit from ubunto on 06/05/2020 at 13:20

--- perf_eval/metric02_resource_evolution/measure_res_evol.py
+++ perf_eval/metric02_resource_evolution/measure_res_evol.py
@@ -6,85 +6,165 @@ import json
 import datetime
 import argparse
 import sys
+import os
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def print_flush(*args, **kwargs):
   kwargs["flush"] = True
   print(*args, **kwargs)
 
 parser = argparse.ArgumentParser()
-  
-parser.add_argument("--service-data", help="Service data to be passed to the request starting the service, default: {}", nargs="?", default="{}")
+
+parser.add_argument("allocation_get", help="Path to the resource allocation request, in the form /<path")
+parser.add_argument("-p", "--preliminary_get", help="Path to the preliminary request, in the form /<path", required=False)
+
+parser.add_argument("--fua-url", help="URL of the Fog User Access, in the form https://<hostname>:<port>", required=True)
+parser.add_argument("--fua-user", help="User for the Fog User Access", required=True)
+parser.add_argument("--fua-password", help="Password for the Fog User Access", required=True) 
+parser.add_argument("--fgw-url", help="URL of the Fog Gateway, in the form https://<hostname>:<port>/<path>", required=False)
+parser.add_argument("--fgw-user", help="User for the Fog Gateway, defaults to Fog User Access user if not specified", required=False)
+parser.add_argument("--fgw-password", help="Password for the Fog Gateway, defaults to Fog User Access password if not specified", required=False)
+parser.add_argument("--fomg-url", help="URL of the FORCH Management, in the form https://<hostname>:<port>/<path>", required=True)
+parser.add_argument("--fomg-user", help="User for the FORCH Management, defaults to Fog User Access user if not specified", required=False)
+parser.add_argument("--fomg-password", help="Password for the Fog Management, defaults to Fog User Access password if not specified", required=False)
+parser.add_argument("-d", "--service-data", help="Service data to be passed to the request starting the service, default: {}", nargs="?", default="{}")
 parser.add_argument("--pre-delete", help="DELETE before starting allocation, default: False", action="store_true", default=False)
 parser.add_argument("--post-delete", help="DELETE after gathering measurements, default: False", action="store_true", default=False)
+parser.add_argument("-y", "--assume-yes", help="Automatic yes to prompts, default: False", action="store_true", default=False)
+
+parser.add_argument("--cycle-delete", help="DELETE at the end of every cycle, default: False", action="store_true", default=False)
+parser.add_argument("-n", "--num-cycles", help="Number of measurement cycles", type=int, nargs="?", default=100)
+
+parser.add_argument("--node-list", help="List of comma-separated Fog Node ID to be activated later", required=False)
+parser.add_argument("--nnic", "--new-node-interval-cycles", help="Activates new node every n cycles", type=int, nargs="?", default=3)
+parser.add_argument("-e", "--end-cycles", help="Number of cycles to perform after node_list is empty", type=int, nargs="?", default=7)
+
+default_output_fname = "res_" + os.path.basename(__file__).split("_")[0] + "_" + "{0:%Y%m%d_%H%M%S}".format(datetime.datetime.now()) + ".json"
+parser.add_argument("-o", "--output-fname", help="Output file name, defaults to {}".format(default_output_fname), nargs="?", default=default_output_fname)
 
 args = parser.parse_args()
 print_flush("CLI args: {}".format(vars(args)))
 
-c = input("Confirm? [y/n] ")
-if c != "y":
-  sys.exit("Aborted.")
+if not args.assume_yes:
+  c = input("Confirm? [y/n] ")
+  if c != "y":
+    sys.exit("Aborted.")
 
 #app_id = args.app_id
+fua_url = args.fua_url.strip("/")
+fua_user = args.fua_user
+fua_password = args.fua_password
+fomg_url = args.fomg_url.strip("/")
+fomg_user = args.fomg_user if args.fomg_user else fua_user
+fomg_password = args.fomg_password if args.fomg_password else fua_password
+if args.fgw_url:
+  fgw_url = args.fgw_url.strip("/")
+  fgw_user = args.fgw_user if args.fgw_user else fua_user
+  fgw_password = args.fgw_password if args.fgw_password else fua_password
 pre_delete = args.pre_delete
 post_delete = args.post_delete
+
+preliminary_get = args.preliminary_get
+allocation_get = args.allocation_get
 service_data_json = json.loads(args.service_data)
+
+cycle_delete = args.cycle_delete
+n_cycles = args.num_cycles
+
+node_id_list = list(args.node_list.split(",")) if args.node_list else []
+new_node_int_cycles = args.nnic
+end_cycles = args.end_cycles
+
+output_fname = args.output_fname
 
 if pre_delete:
   #curl -X DELETE http://127.0.0.1:5003/nodes
-  print_flush("DELETE", "http://127.0.0.1:5003/nodes")
-  r = requests.delete("http://127.0.0.1:5003/nodes")
-  print_flush("Sleep for 240 seconds")
+  url = fomg_url + "/127.0.0.1/5003/nodes"
+  print_flush("DELETE", url)
+  r = requests.delete(url, auth=(fomg_user, fomg_password), verify=False)
+
+  print_flush("Sleep for 4 minutes")
   time.sleep(240)
 
-#curl http://192.168.10.117:5001/apps
-print_flush("GET", "http://192.168.10.117:5001/nodes")
-r = requests.get("http://192.168.10.117:5001/apps")
-time.sleep(1)
+# start measuring
 
-# request the allocation of new service
-print_flush("GET", "http://192.168.10.117:5001/app/APP002")
-r = requests.get("http://192.168.10.117:5001/app/APP002")
-resp_json = r.json()
-print_flush(json.dumps(resp_json, indent=2))
-time.sleep(1)
+print_flush("\n### START MEASUREMENTS ###\n")
 
-while r.status_code in [200, 201]:
-  # gather data on deployed service
-  node_ip = resp_json["node_ip"]
-  serv_port = resp_json["service_port"]
+remaining_cycles = n_cycles
+i = 0
 
-  # start new service
-  url = "http://{}:{}/app/APP002".format(node_ip, serv_port)
-  data_json = service_data_json
-  print_flush("POST", data_json, url)
-  r = requests.post(url, json=data_json)
-  resp_json = r.json()
-  print_flush(json.dumps(resp_json, indent=2))
-  time.sleep(1)
+while remaining_cycles > 0:
 
-  print_flush("Sleep for 120 seconds")
-  time.sleep(120)
+  # increment cycle index
+  i += 1
+
+  print_flush("\n# Cycle {} ({} remaining)\n".format(i, remaining_cycles))
+
+  if preliminary_get:
+    url = fua_url + preliminary_get
+    print_flush("GET", url)
+    r = requests.get(url, auth=(fua_user, fua_password), verify=False)
+    time.sleep(1)
 
   # request the allocation of new service
-  print_flush("GET", "http://192.168.10.117:5001/app/APP002")
-  r = requests.get("http://192.168.10.117:5001/app/APP002")
+  url = fua_url + allocation_get
+  print_flush("GET", url)
+  r = requests.get(url, auth=(fua_user, fua_password), verify=False)
   resp_json = r.json()
   print_flush(json.dumps(resp_json, indent=2))
-  time.sleep(10)
 
-print_flush("Sleep for 240 seconds")
-time.sleep(240)
+  time.sleep(1)
 
-# get measurements
-print_flush("GET", "http://127.0.0.1:5003/meas")
-r = requests.get("http://127.0.0.1:5003/meas")
-time.sleep(1)
-with open("../gauchotest/get_meas_test_{0:%Y%m%d_%H%M%S}.json".format(datetime.datetime.now()), "w") as f:
-  json.dump(r.json(), f)
+  # if allocation was successful, activate new service
+  if r.status_code  in [200, 201]:
+    node_id = resp_json["node_id"]
+    serv_port = resp_json["service_port"]
+    url = "{}/{}/{}/{}".format(fgw_url, node_id, serv_port, allocation_get.strip("/").replace("/", "-"))
+    print_flush("POST", url)
+    r = requests.post(url, json=service_data_json, auth=(fgw_user, fgw_password), verify=False)
+    resp_json = r.json()
+    print_flush(json.dumps(resp_json, indent=2))
+
+    time.sleep(1)
+
+  # on some cycles activate a new fog node
+  if i % new_node_int_cycles == 0 and node_id_list:
+    node_id = node_id_list.pop(0)
+    url = fgw_url + "/" + node_id + "/8000"
+    print_flush("GET", url)
+    r = requests.get(url, auth=(fgw_user, fgw_password), verify=False)
+    if not node_id_list:
+      print_flush("WARNING: node_id_list is empty")
+      remaining_cycles = end_cycles
+
+  if cycle_delete:
+    url = fomg_url + "/127.0.0.1/5003/nodes"
+    print_flush("DELETE", url)
+    r = requests.delete(url, auth=(fomg_user, fomg_password), verify=False)
+
+    time.sleep(1)
+
+  remaining_cycles -= 1
+  
+  print_flush("Sleep for 2 minutes")
+  time.sleep(120)
 
 if post_delete:
-  #curl -X DELETE http://127.0.0.1:5003/nodes
-  print_flush("DELETE", "http://127.0.0.1:5003/nodes")
-  r = requests.delete("http://127.0.0.1:5003/nodes")
+  url = fomg_url + "/127.0.0.1/5003/nodes"
+  print_flush("DELETE", url)
+  r = requests.delete(url, auth=(fomg_user, fomg_password), verify=False)
   time.sleep(1)
+
+# get measurements
+url = fomg_url + "/127.0.0.1/5003/meas"
+print_flush("GET", url)
+r = requests.get(url, auth=(fomg_user, fomg_password), verify=False)
+#print_flush(json.dumps(meas_dict, indent=2))
+
+with open(output_fname, "w") as f:
+  json.dump(r.json(), f)
+  
+print_flush("Measurement data written to file {}".format(output_fname))
 
---
+++ perf_eval/metric02_resource_evolution/pp_meas.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+
+import sys
+
+import json
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+import argparse 
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("fname", help="File name")
+parser.add_argument("-i", "--ignore-last", help="Number of trailing results to ignore, starting from most recent, default: 0", type=int, nargs="?", default=0)
+parser.add_argument("-l", "--limit", help="Maximum number of results to show, starting from most recent, default: 1000", type=int, nargs="?", default=1000)
+parser.add_argument("-f", "--fontsize", help="Font size, default: 10", type=int, nargs="?", default=10)
+parser.add_argument("-w", "--widthfactor", help="Width factor, default: 0.9", type=float, nargs="?", default=0.9)
+parser.add_argument("-s", "--figsizes", help="Sizes of the figure in inches, comma-separated values, default: 8,6", nargs="?", default="8,6")
+parser.add_argument("-d", "--figdpi", help="DPI of the figure, default: 40", nargs="?", type=int, default=40)
+parser.add_argument("--legend", help="Legend formatted as comma separated host_id:name (e.g., 10313:NUC1,10316:RP1), default: none [use host_id]", nargs="?", default="")
+parser.add_argument("--show", help="Show plot, default: False", action="store_true", default=False)
+
+args = parser.parse_args()
+print("CLI args: {}".format(vars(args)))
+
+fname = args.fname
+ignore_last = args.ignore_last
+limit = args.limit
+fontsize = args.fontsize
+width_factor = args.widthfactor
+fig_width = int(args.figsizes.split(",")[0])
+fig_height = int(args.figsizes.split(",")[1])
+fig_dpi = args.figdpi
+legend_str = args.legend
+plt_show = args.show
+
+# set font size
+plt.rcParams.update({"font.size": fontsize})
+
+with open(fname) as f:
+  meas_dict = json.load(f)
+
+sorted_t = sorted(meas_dict.keys())
+
+# apply ignore_last
+if ignore_last > 0:
+  sorted_t = sorted_t[:-ignore_last]
+# apply limit
+if len(sorted_t) > limit:
+  sorted_t = sorted_t[-limit:]
+
+# process the legend
+legend = {}
+if legend_str:
+  for pair in legend_str.split(","):
+    h_id, name = pair.split(":")
+    legend[h_id] = name
+else:
+  legend = { k:k for k in meas_dict[sorted_t[0]].keys() }
+print("legend", legend)
+
+# sort host_id and flter out those not included in the legend
+sorted_host_id = [ h_id for h_id in sorted(meas_dict[sorted_t[0]].keys()) if h_id in legend ]
+n_hosts = len(sorted_host_id)
+print(n_hosts, sorted_host_id)
+
+patterns = [ "//", "\\", "+", "x", "|", "/", "-", ".", "o", "O", "*" ]
+
+plot_dict = { h: {} for h in sorted_host_id }
+
+lastclock_dict = {}
+
+for t in sorted_t:
+  meas = meas_dict[t]
+  for host_id in sorted_host_id:
+    item_dict = meas[host_id]
+    lastclock_dict[host_id] = 0
+    for item_id in item_dict:
+      item = item_dict[item_id]
+      if item["name"] == "CPU utilization":
+        #print(t, host_id, item["lastvalue"])
+        if item["lastclock"] != lastclock_dict[host_id]:
+          lastclock_dict[host_id] = item["lastclock"]
+          plot_dict[host_id][item["lastclock"]] = round(float(item["lastvalue"]))
+
+# trim length of plot_dict
+min_length = min( [ len(plot_dict[h_id]) for h_id in plot_dict ] )
+for h_id in plot_dict:
+  while len(plot_dict[h_id]) > min_length:
+    last_t = max(plot_dict[h_id].keys())
+    del plot_dict[h_id][last_t]
+
+# create x axis labels
+labels = [ str(t) for t in range(min_length) ]
+
+# display information
+print("labels", len(labels), labels)
+for h_id in plot_dict:
+  print("h_id", h_id)
+  #print("plot_dict", plot_dict[h_id])
+  print("plot_dict keys", len(plot_dict[h_id].keys()), plot_dict[h_id].keys())
+  print("plot_dict values", len(plot_dict[h_id].values()), plot_dict[h_id].values())
+
+x = np.arange(len(labels))  # the label locations
+width = width_factor/n_hosts  # the width of the bars
+
+fig, ax = plt.subplots()
+
+#if n_hosts == 1:
+#  rect_list = [ ax.bar(x, plot_dict[sorted_host_id[0]], width, label=sorted_host_id[0]) ]
+#else:
+#  rect_list = [ ax.bar(x - (n_hosts-1)/2 * width + i*width, plot_dict[sorted_host_id[i]], width, label=sorted_host_id[i]) for i in range(n_hosts) ]
+
+rect_list = [ ax.bar( np.arange(len(plot_dict[sorted_host_id[i]].values())) - (n_hosts-1)/2 * width + i*width, list(plot_dict[sorted_host_id[i]].values()), width, label=legend[sorted_host_id[i]], hatch=patterns[i], alpha=.99) for i in range(n_hosts) ]
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+#ax.set_title("CPU utilization")
+ax.set_xlabel("Time [min]")
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.set_ylabel("CPU utilization [%]")
+ax.set_ylim([0, 120])
+ax.legend()
+
+
+def autolabel(rects):
+  """Attach a text label above each bar in *rects*, displaying its height."""
+  for rect in rects:
+    height = rect.get_height()
+    ax.annotate('{}'.format(height),
+      xy=(rect.get_x() + rect.get_width() / 2, height),
+      xytext=(0, 3),  # 3 points vertical offset
+      textcoords="offset points",
+      ha='center', va='bottom')
+
+#for rect in rect_list:
+#  autolabel(rect)
+
+fig.tight_layout()
+#plt.margins(0)
+
+fig.set_size_inches(fig_width, fig_height)
+fig.set_dpi(fig_dpi)
+
+if plt_show:
+  plt.show()
+
+plt.savefig('out.pdf', bbox_inches='tight')
+
