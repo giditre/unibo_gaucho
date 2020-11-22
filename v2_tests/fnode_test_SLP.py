7faf9c1526a52aac713ac55532114dae0e2aa3e8
student <giditre@gmail.com>
2020-11-22 11:13:36 +0000

    Changed v2_tests files

--- v2_tests/fnode_test_SLP.py
+++ v2_tests/fnode_test_SLP.py
@@ -1,16 +1,18 @@
 import sys
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent.parent.absolute()))
+sys.path.append(str(Path(__file__).parent.parent.absolute()))
+print(sys.path)
 
 from forch.forch_utils_service import Service
 from forch.forch_utils_slp import SLPFactory
 from ipaddress import IPv4Address
-
+import asyncio
 import time
 
 sa = SLPFactory.create_SA()
 srv_list = Service.create_services_from_json(IPv4Address("192.168.10.123"), str(Path(__file__).parent.joinpath("service_example.json").absolute()))
 for srv in srv_list:
+  print(srv)
   sa.register_service(srv)
 
-
+asyncio.get_event_loop().run_forever()
