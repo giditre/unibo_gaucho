726d5778aa7e04dd72d0b14af16909aaf357acd1
giditre <giditre@gmail.com>
2020-11-28 19:28:55 +0100

    detailed comments on FOB, to be implemented

--- main/forch_main.py
+++ main/forch_main.py
@@ -15,6 +15,7 @@ forch.set_orchestrator()
 
 logger.debug("IS_ORCHESTRATOR: {}".format(forch.is_orchestrator()))
 
+
 class FOB(object):
   __key = object()
   __instance = None
@@ -27,6 +28,50 @@ class FOB(object):
     if cls.__instance is None:
       cls.__instance = cls(key=cls.__key)
     return cls.__instance
+
+  @staticmethod
+  def get_service_list():
+    return FORS.get_instance().get_service_list()
+
+  @staticmethod
+  def allocate(s_id):
+    """Takes service ID and returns a Service object or None."""
+    s_list = FORS.get_instance().get_service_list()
+    s_id_list = [ s.get_id() for s in s_list ]
+    if s_id in s_id_list:
+      # need to check which node is best suited to host the service
+      # first get instance of requested service
+      s = next(s for s in s_list if s.get_id() == s_id)
+      # then use retrieve_measurement to populate metrics with measurements
+      # TODO this is a responsibility of FOVIM
+      s.retrieve_measurements()
+      # then pick most suitable node
+      sn = s.get_node_by_metric() # by default returns node with minimum CPU utilization
+      # TODO check if best node is compliant with constraints (e.g.: if min CPU is lower than threshold for allocation)
+      if True:
+        # if yes, trigger the requested allocation through FOVIM
+        FOVIM.get_instance().process_allocation(service_id=s_id, node_id=sn.get_id())
+        # TODO: get response and return it to user --> 200 OK
+      else:
+        # if no, service might be deployed on a new node, so continue
+        pass
+    
+    # we get here if service is not deployed (or unknown), or deployed but on nodes that are too busy
+    # check if service is deployable (e.g.: "by deploying an APP on a IaaS node")
+    # TODO gather list of available sources (SDP codeblocks and FVE images) from FOVIM (reword explanation)
+
+    # TODO check if there is a source that offers the requested service
+
+      # TODO if yes, check if there is a node that offers the required SDP/FVE for the source, and is free enough to host it ( get_node_by_metric() )
+      
+        # TODO if yes deploy the source and allocate service on it --> 201 Created
+
+        # TODO if no, there are no more resources for new deployments --> 503 Service Unavailable
+
+      # TODO if no, impossible to deploy service in virtualized way (or unknown service) --> 501 Not Implemented
+
+    
+
 
 class FORS(object):
   __key = object()
@@ -64,6 +109,12 @@ class FOVIM(object):
       cls.__instance = cls(key=cls.__key)
     return cls.__instance
 
+  @staticmethod
+  def process_allocation(*, service_id, node_id):
+    # check if service is already deployed (e.g.: an APP offered by a SaaS node)
+    # 
+    pass
+
 ### start components
 
 FORS.get_instance()
@@ -80,51 +131,54 @@ class Test(Resource):
 
 class FogServices(Resource):
   def get(self, s_id=""):
-    s_list = FORS.get_instance().get_service_list()
+    """Gather list of services and format it in a response."""
+    # get list of services
+    s_list = FOB.get_instance().get_service_list()
+    # format response based on request
     s_id_list = [ s.get_id() for s in s_list ]
+    # check if a service was specified
     if s_id:
       if s_id in s_id_list:
         return {
           "message": f"Requested service {s_id} found.",
-          "type": "FOCO_SERV_OK",
+          # "type": "FOCO_SERV_OK",
           "services": [ s_id ]
         }
       else:
         return {
           "message": f"Requested service {s_id} not found.",
-          "type": "FOCO_SERV_LIST",
+          # "type": "FOCO_SERV_LIST",
           "services": []
         }, 404
     else:
       return {
         "message": f"Found {len(s_list)} service(s).",
-        "type": "FOCO_SERV_LIST",
+        # "type": "FOCO_SERV_LIST",
         "services": [ s.get_id() for s in s_list ]
       }
 
   def post(self, s_id):
-    s_list = FORS.get_instance().get_service_list()
-    s_id_list = [ s.get_id() for s in s_list ]
-    if s_id in s_id_list:
-      # need to check which node is best suited to host the service
-      # first get instance of requested service
-      s = next(s for s in s_list if s.get_id() == s_id)
-      # then use retrieve_measurement to populate metrics with measurements
-      s.retrieve_measurements()
-      # then pick most suitable node
-      sn = s.get_node_by_metric() # by default returns node with minimum CPU utilization
-      # TODO: instead of returning, trigger the requested allocation
+    """Submit request for allocation of a service."""
+    s = FOB.get_instance().allocate(s_id)
+    if s is None:
+      # service not found
       return {
-        "message": f"Service {s} allocated on node {sn}.",
-        "type": "FOCO_SERV_POST"
-      }
-    else:
-      # service not available
-      return {
-          "message": f"Requested service {s_id} not found.",
-          "type": "FOCO_SERV_POST",
-          "services": []
+          "message": f"Requested service {s_id} not found."
+          # "type": "FOCO_SERV_POST",
+          # "services": []
         }, 404
+    elif isinstance(s, forch.Service):
+      assert len(s.get_node_list()) in [0,1], "Too many ServiceNodes!"
+      if len(s.get_node_list()) == 0:
+        return {
+        "message": f"Service {s.get_id()} unavailable."
+        # "type": "FOCO_SERV_POST"
+        }, 503
+      elif len(s.get_node_list()) == 1:
+        return {
+          "message": f"Service {s.get_id()} allocated on node {s.get_node_list()[0]}."
+          # "type": "FOCO_SERV_POST"
+        }, 201
 
 if __name__ == '__main__':
   ### Command line argument parser
