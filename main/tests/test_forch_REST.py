37e005d203d7b14d6ad8ac07a061e2467db20470
giditre <giditre@gmail.com>
2020-12-03 14:44:49 +0000

    edited forch REST test with test_client

--- main/tests/test_forch_REST.py
+++ main/tests/test_forch_REST.py
@@ -1,43 +1,66 @@
+import flask
+import flask_restful
+
 import forch
 
 from src.main.forch_main import FogServices
 
 def test_services_get_200():
-  resource = FogServices()
-  assert isinstance(resource, FogServices), ""
-  response, code = resource.get()
-  assert isinstance(response, dict), f"Response: {response}"
-  assert isinstance(code, int), f"Response code: {code}"
-  assert code == 200, f"Response: {response}, response code: {code}"
+  app = flask.Flask(__name__)
+  app.testing = True
+  api = flask_restful.Api(app)
+  api.add_resource(FogServices, '/services', '/services/<s_id>')
+  with app.test_client() as tc:
+    response = tc.get("/services")
+    resp_json = response.get_json(force=True)
+    assert isinstance(resp_json, dict), f"Response: {resp_json}"
+    code = response.status_code
+    assert code == 200, f"Response code: {code}"
 
 def test_services_get_single_200():
-  resource = FogServices()
-  assert isinstance(resource, FogServices), ""
-  response, code = resource.get("APP001")
-  assert isinstance(response, dict), f"Response: {response}"
-  assert isinstance(code, int), f"Response code: {code}"
-  assert code == 200, f"Response: {response}, response code: {code}"
+  app = flask.Flask(__name__)
+  app.testing = True
+  api = flask_restful.Api(app)
+  api.add_resource(FogServices, '/services', '/services/<s_id>')
+  with app.test_client() as tc:
+    response = tc.get("/services/APP001")
+    resp_json = response.get_json(force=True)
+    assert isinstance(resp_json, dict), f"Response: {resp_json}"
+    code = response.status_code
+    assert code == 200, f"Response code: {code} - is SLP running?"
 
 def test_services_get_404():
-  resource = FogServices()
-  assert isinstance(resource, FogServices), ""
-  response, code = resource.get("APP999")
-  assert isinstance(response, dict), f"Response: {response}"
-  assert isinstance(code, int), f"Response code: {code}"
-  assert code == 404, f"Response: {response}, response code: {code}"
+  app = flask.Flask(__name__)
+  app.testing = True
+  api = flask_restful.Api(app)
+  api.add_resource(FogServices, '/services', '/services/<s_id>')
+  with app.test_client() as tc:
+    response = tc.get("/services/asdasd")
+    resp_json = response.get_json(force=True)
+    assert isinstance(resp_json, dict), f"Response: {resp_json}"
+    code = response.status_code
+    assert code == 404, f"Response code: {code}"
 
 def test_services_post_200():
-  resource = FogServices()
-  assert isinstance(resource, FogServices), ""
-  response, code = resource.post("APP001")
-  assert isinstance(response, dict), f"Response: {response}"
-  assert isinstance(code, int), f"Response code: {code}"
-  assert code == 200, f"Response: {response}, response code: {code}"
+  app = flask.Flask(__name__)
+  app.testing = True
+  api = flask_restful.Api(app)
+  api.add_resource(FogServices, '/services', '/services/<s_id>')
+  with app.test_client() as tc:
+    response = tc.post("/services/APP001")
+    resp_json = response.get_json(force=True)
+    assert isinstance(resp_json, dict), f"Response: {resp_json}"
+    code = response.status_code
+    assert code == 200, f"Response code: {code} - is SLP running?"
 
 def test_services_post_404():
-  resource = FogServices()
-  assert isinstance(resource, FogServices), ""
-  response, code = resource.post("APP999")
-  assert isinstance(response, dict), f"Response: {response}"
-  assert isinstance(code, int), f"Response code: {code}"
-  assert code == 404, f"Response: {response}, response code: {code}"
+  app = flask.Flask(__name__)
+  app.testing = True
+  api = flask_restful.Api(app)
+  api.add_resource(FogServices, '/services', '/services/<s_id>')
+  with app.test_client() as tc:
+    response = tc.post("/services/asdasd")
+    resp_json = response.get_json(force=True)
+    assert isinstance(resp_json, dict), f"Response: {resp_json}"
+    code = response.status_code
+    assert code == 404, f"Response code: {code}"
