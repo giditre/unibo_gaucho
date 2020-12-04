ef6c12d08a91506d9540aa4b245a38a41ec0f1bf
giditre <giditre@gmail.com>
2020-12-04 12:54:08 +0100

    more work on deployment and log calls

--- main/tests/test_forch_FOVIM.py
+++ main/tests/test_forch_FOVIM.py
@@ -1,3 +1,5 @@
+import requests
+
 import forch
 
 from src.main.forch_main import FOVIM
@@ -12,15 +14,22 @@ def test_manage_allocation():
   FOVIM.del_instance()
 
 def test_manage_deployment():
+  node_ip = "192.168.64.123"
   src = {
       "id": "SRC002",
       "name": "httpd",
       "descr": "Apache web server",
       "uri": "httpd",
       "base": "FVE001",      
-      "service": "APP001"
+      "service": "APP001",
+      "ports": ["80/tcp"]
     }
-  s = FOVIM.get_instance().manage_deployment(service_id="APP001", node_ip="192.168.64.123", source=src)
+  s = FOVIM.get_instance().manage_deployment(service_id="APP001", node_ip=node_ip, source=src)
+  FOVIM.del_instance()
   assert s is not None, str(s)
   assert isinstance(s, forch.Service), str(s)
-  FOVIM.del_instance()
+  assert len(s.get_node_list()) == 1, str(s)
+  sn = s.get_node_list()[0]
+  sn_port = sn.get_port()
+  response = requests.get(f"http://{node_ip}:{sn_port}")
+  assert response.status_code == 200, str(response.text)
