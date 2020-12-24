8215a22f2c37060f84f8877b7376c043716e9d3d
mrv96 <mrv96@users.noreply.github.com>
2020-12-24 12:56:25 +0100

    Update fo_slp.py

--- pyforch/src/forch/fo_slp.py
+++ pyforch/src/forch/fo_slp.py
@@ -138,7 +138,7 @@ class SLPFactory:
     def __get_daemon_pid():
       out = subprocess.run(shlex.split("pgrep slpd"), capture_output=True).stdout.decode('utf-8')
       if not out:
-        return None
+        return None # TODO: maybe it is better to raise an error
       assert out.count("\n") == 1, "Found more than one slpd PID!"
       return int(out)
 
