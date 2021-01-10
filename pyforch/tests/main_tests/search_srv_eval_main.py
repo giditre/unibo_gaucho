e016120e3953f1d4ff84f7c17d85fc79e5b0a350
mrv96 <mrv96@noreply.github.com>
2021-01-10 22:37:52 +0100

    Final search services evaluation implementation

--- pyforch/tests/main_tests/search_srv_eval_main.py
+++ pyforch/tests/main_tests/search_srv_eval_main.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 import os
 import sys
-from typing import Any, List
+from typing import List
 
-from pyshark.capture.capture import StopCapture
+from scapy.packet import Packet
+from scapy.utils import wrpcap
 sys.path.append("/home/gaucho/mario/unibo_gaucho/pyforch")
 # print(sys.path)
 # print(sys.modules)
@@ -11,8 +12,9 @@ sys.path.append("/home/gaucho/mario/unibo_gaucho/pyforch")
 from src.forch.fo_service import Service
 from src.forch.fo_slp import SLPFactory
 from src.forch.fo_servicecache import ServiceCache
+from src.forch import raise_error
 
-import pyshark
+from scapy.all import AsyncSniffer
 
 from pathlib import Path
 import argparse
@@ -21,8 +23,6 @@ from ipaddress import IPv4Address
 import time # https://stackoverflow.com/questions/5849800/what-is-the-python-equivalent-of-matlabs-tic-and-toc-functions
 import netifaces as ni
 
-from collections import deque
-import threading
 import csv
 
 class TicToc:
@@ -56,53 +56,13 @@ class TicToc:
     self.toc(print_out=True)
 
 
-class Sniffer(threading.Thread):
-  def __init__(self, ifaces:List[str], timeout:int|None=None, out_file:str|bool=False):
-    super().__init__()
-    self.__timeout = timeout
-    self.__stop: bool = False
-    self.__ifaces: List[str] = ifaces
-    self.__pkts: List[Any] = [] # list of packets
-    self.__out_file: str|bool = out_file
-    self.__cap: pyshark.LiveCapture
-
-  def get_packets_list(self):
-    return self.__pkts # Is it better to use threading.Lock() and perform a deepcopy?
-
-  def stop(self):
-    self.__stop = True
-
-  def __load_packets(self, packet_count=0, timeout=None):
-    def keep_packet(pkt):
-      self.__pkts.append(pkt)
-      if self.__stop == True:
-        raise StopCapture()
-
-    try:
-      self.__cap.apply_on_packets(keep_packet, timeout=timeout, packet_count=packet_count)
-    except:
-      pass
-
-  def run(self):
-    self.__stop = False
-    self.__pkts = []
-    if isinstance(self.__out_file, str):
-      self.__cap = pyshark.LiveCapture(interface=self.__ifaces, output_file=self.__out_file)#, bpf_filter='port 427||port 1847||multicast', display_filter='srvloc')
-    elif self.__out_file:
-      self.__cap = pyshark.LiveCapture(interface=self.__ifaces, output_file="tst.pcap")#, bpf_filter='port 427||port 1847||multicast', display_filter='srvloc')
-    else:
-      self.__cap = pyshark.LiveCapture(interface=self.__ifaces)#, bpf_filter='port 427||port 1847||multicast', display_filter='srvloc')
-    self.__load_packets(timeout=self.__timeout)
-
-
-
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Test FORCH OpenSLP implementation.')
   # Optional arguments
   parser.add_argument('-m', '--mode', choices=['c', 'd'], default='d', help='Specify SLP working mode: c (centralized) or d (distributed).')
   parser.add_argument('-i', '--ifaces', nargs="+", help='Specify the inet ifaces where run SLP.')
   parser.add_argument('-a', '--ipaddrs', nargs="+", help='Specify the ip addresses where run SLP.')
-  parser.add_argument('-j', '--files', nargs="+", default=['../service_example.json'], help='Specify the service JSON files to be used.')
+  parser.add_argument('-j', '--files', nargs="+", default=['/home/gaucho/mario/unibo_gaucho/pyforch/tests/main_tests/srvcs_1.json'], help='Specify the service JSON files to be used.') # In a hypotetic final version, the default field should be replaced with required=True
   parser.add_argument('-t', '--times', type=int, default=1, help='Specify the number of trials to be done')
   # Mandatory arguments
   parser.add_argument('dev', choices=['o', 'n'], help='Specify the device type: o (for orchestrator) or n (for node).')
@@ -145,59 +105,101 @@ if __name__ == "__main__":
       sa.register_service(srv)
     asyncio.get_event_loop().run_forever()
   else:
-    all_res = [['TRIAL TYPE: {}+{}'.format(args.dev, args.mode), 'TIME', 'N_PKTS', 'TOT_BYTES', 'DATA_CONSISTENCY']]
-    da = None
+    da: SLPFactory.__DirectoryAgent|None = None
+    not_passed_flag: bool = False
+    all_res: List[List[str]] = [['TRIAL TYPE: {}+{}'.format(args.dev, args.mode), 'TIME', 'N_PKTS', 'TOT_BYTES', 'DATA_CONSISTENCY']]
 
     for i in range(args.times):
+      monitor: AsyncSniffer = AsyncSniffer(iface=iface_list, filter='port 427 || port 1847')
+      tt: TicToc
+      sc: ServiceCache
       fnd: List[Service] = []
       elapsed_time: float = -1
+      pkts_list: List[Packet] = []
       n_pkts: int = -1
       tot_bytes: int = 0
+      
+      monitor.start()
+      time.sleep(0.1)
+
+      sc = ServiceCache()
 
       if args.mode == 'd':
-        monitor = Sniffer(ifaces=iface_list, timeout=5, out_file=('test{}.pcap'.format(i)))
-        tt = TicToc("Test SC Refresh #{}".format(i))
-
-        monitor.start() # TODO: mettere uno sleep di sicurezza dopo questa istruction?
-        sc = ServiceCache()
-
-        tt.tic()
-        sc.refresh()
-        elapsed_time = tt.toc()
-
-        monitor.stop()
-        monitor.join()
-        pkts_list = monitor.get_packets_list()
-
-        fnd = sc.get_list()
+        tt = TicToc("Test Distribuited Mode #{}".format(i))
       else:
-        monitor = Sniffer(ifaces=iface_list, timeout=5, out_file=('test{}.pcap'.format(i)))
-        monitor.start() # TODO: mettere uno sleep di sicurezza dopo questa istruction? Metterlo qua o dopo il DA in modo da avere timeout più corti?
+        tt = TicToc("Test Centralized Mode #{}".format(i))
+
         if da is None:
           da = SLPFactory.create_DA(new_handler=True)
           input('DA started. Press enter to find services...')
+        
+      tt.tic()
+      sc.refresh()
+      elapsed_time = tt.toc()
 
-        ua = SLPFactory.create_UA()
-        tt = TicToc("Test UA Find Srvs #{}".format(i))
-        tt.tic()
-        fnd = ua.find_all_services()
-        elapsed_time = tt.toc()
+      fnd = sc.get_list()
 
-        monitor.stop()
-        monitor.join()
-        pkts_list = monitor.get_packets_list()
+      time.sleep(0.1)
+      pkts_list = monitor.stop()
+      wrpcap(filename='./res/test{}.pcap'.format(i), pkt=pkts_list)
 
       n_pkts = len(pkts_list)
       for pkt in pkts_list:
-        tot_bytes += pkt.length
+        tot_bytes += len(pkt)
       print('Number of received packets: {}'.format(n_pkts))
       print('Total packets length [bytes]: {}'.format(tot_bytes))
 
-      passed = str(all([el for i, el in enumerate([ann.__dict__ == fnd[fnd.index(ann)].__dict__ for ann in srv_list]) if i != 2]))
-      
-      all_res.append([str(i), str(elapsed_time), str(n_pkts), str(tot_bytes), str(passed)])
-      # assert all([el for i, el in enumerate([ann.__dict__ == fnd[fnd.index(ann)].__dict__ for ann in srv_list]) if i != 2]), "Some found service is different from the registered one"
+      passed: bool = True
+      for ann in srv_list:
+        try:
+          fnd_dict = fnd[fnd.index(ann)].__dict__
+        except:
+          print(1)
+          input()
+          passed = False
+          break
+        for key in ann.__dict__:
+          if key != "_Service__node_list":
+            try:
+              if ann.__dict__[key] != fnd_dict[key]:
+                print(2)
+                input()
+                passed = False
+                break
+            except:
+              print(3)
+              input()
+              passed = False
+              break
+          else:
+            for j, node in enumerate(ann.__dict__[key]):
+              for key2 in node.__dict__:
+                if key2 != "_ServiceNode__id" and key2 != "_ServiceNode__ip" and key2 != "_ServiceNode__lifetime":
+                  try:
+                    if node.__dict__[key2] != fnd_dict[key][j-1].__dict__[key2]:
+                      print(4)
+                      input()
+                      passed = False
+                      break
+                  except:
+                    print(5)
+                    input()
+                    passed = False
+                    break
+        if not passed:
+          break
 
-    with open('res.csv', mode='w') as res_file:
+      if not passed and not not_passed_flag:
+        not_passed_flag = True
+      
+      # assert passed, "Some found service is different from the registered one"
+      all_res.append([str(i), str(elapsed_time), str(n_pkts), str(tot_bytes), str(passed)])
+
+    # The file is written only at the end. This means that if the script is stopped before there will not be intermediate works available.
+    # This could be an issue to be solved if needed. For the moment i prefer to access to the file once.
+    with open('./res/res.csv', mode='w') as res_file:
       res_writer = csv.writer(res_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
       res_writer.writerows(all_res)
+
+    if not_passed_flag:
+      raise_error("__main__", "At least one found service is not correct!")
